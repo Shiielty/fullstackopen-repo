81331f900cbb7d540f440bad7085101400225b50
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-08-02 22:51:25 +0700

    Finished exercise 1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  );
+};
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div className="App">
+      <h2>give feedback</h2>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+}
+
+export default App;
