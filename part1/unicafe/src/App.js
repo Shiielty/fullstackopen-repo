c37d997eb3e35ca949264206e65afc1ed44e2259
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-08-02 23:28:47 +0700

    Finished exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,7 +5,12 @@ const Button = ({ text, handleClick }) => {
 };
 
 const StatisticLine = ({ text, value }) => {
-  return <p>{text} {value}</p>;
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
 };
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -23,12 +28,16 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positivePercentage} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positivePercentage} />
+        </tbody>
+      </table>
     </div>
   );
 };
