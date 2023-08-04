1d09beb22a66caa451095f6a1c372fad33a3b68f
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-08-04 11:55:17 +0700

    Finished exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,15 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const [filteredPerson, setFilteredPerson] = useState([...persons]);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -13,6 +18,20 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+    const query = event.target.value;
+    const regex = RegExp(query);
+    const matches = persons.filter((person) =>
+      person.name.toLowerCase().match(regex),
+    );
+    if (query === "") {
+      setFilteredPerson(persons);
+    } else {
+      setFilteredPerson(matches);
+    }
   };
 
   const checkSameName = (name) => {
@@ -34,6 +53,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -46,7 +69,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {filteredPerson.map((person) => (
         <div key={person.name}>
           {person.name} {person.number}
         </div>
